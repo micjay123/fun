3e60a183bf1bb241a31a76bcb7449ff7c767b02f
wolf <mic_jay@icloud.com>
2021-05-16 17:02:33 +0800

    Initial commit

---
+++ src/main/java/com/mix/fun/FunApplication.java
@@ -0,0 +1,14 @@
+package com.mix.fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunApplication.class, args);
+        System.out.println("=======启动成功========");
+    }
+
+}
